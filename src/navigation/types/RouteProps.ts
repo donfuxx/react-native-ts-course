b9ef83bdcc597f03761cfc525a9f15a1dee2da8d
Jemal Ahmedov <jemaljahmedov@gmail.com>
2020-12-12 13:10:58 +0000

    Fix lint

--- src/navigation/types/RouteProps.ts
+++ src/navigation/types/RouteProps.ts
@@ -1,10 +1,13 @@
-import { RouteProp } from "@react-navigation/native";
-import { Destination } from "./Destination";
-import { AppStackParamList } from "./RouteParams";
+import {RouteProp} from '@react-navigation/native';
+import {Destination} from './Destination';
+import {AppStackParamList} from './RouteParams';
 
 export type SearchResultsRouteProp = RouteProp<
   AppStackParamList,
   Destination.SearchResults
 >;
 
-export type PropertyRouteProp = RouteProp<AppStackParamList, Destination.Property>;
+export type PropertyRouteProp = RouteProp<
+  AppStackParamList,
+  Destination.Property
+>;
